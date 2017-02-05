f3b4f6c57800993052a5eb93ff7ac78946e2d5ff
Hojjat <hojjat.reyhane@yahoo.com>
2017-02-05 09:18:57 +0330

    Add project files

---
+++ Assets/bobardo IAB/InAppStore.cs
@@ -0,0 +1,206 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* Apache License. Copyright (C) Bobardo Studio - All Rights Reserved.
+ * Unauthorized publishing the plugin with different name is strictly prohibited.
+ * This plugin is free and no one has right to sell it to others.
+ * http://bobardo.com
+ * http://opensource.org/licenses/Apache-2.0
+ */
+
+[RequireComponent(typeof(StoreHandler))]
+public class InAppStore : MonoBehaviour
+{
+    public Product[] products;
+
+    public Text txtResult;
+    public Text coinTxt;
+    public GameObject successDialog, errorDialog;
+    public Button btnDoubleCoin;
+    public Text txtSuccessDialog;
+    public Text txtErrorDialog;
+
+    private int coin = 0;
+    private bool doubleCoin;
+
+    private int selectedProductIndex;
+
+    void Start()
+    {
+        coin = PlayerPrefs.GetInt("coin", 0);
+        coinTxt.text = coin + "$";
+        doubleCoin = (PlayerPrefs.GetInt("doubleCoin", 0) == 1);
+        btnDoubleCoin.interactable = !doubleCoin;
+    }
+
+    public void purchasedSuccessful(Purchase purchase)
+    {
+        // purchase was successful, give user the pruduct
+        txtResult.text = "purchased successful. product: " + purchase.productId + " , token: " + purchase.orderId + "\n" + txtResult.text;
+
+        switch (selectedProductIndex)
+        {
+            case 0: // 500 coin
+                saveCoin(500);
+                break;
+            case 1: // enable double coin
+                doubleCoin = true;
+                PlayerPrefs.SetInt("doubleCoin", 1);
+                btnDoubleCoin.interactable = false;
+                break;
+            default:
+                throw new UnassignedReferenceException("you forgot to give user the product after purchase. product: " + purchase.productId);
+        }
+
+        txtSuccessDialog.text = "Thanks for purchasing.";
+        successDialog.SetActive(true);
+    }
+
+    public void purchasedFailed(int errorCode, string info)
+    {
+        // purchase failed. show user the proper message
+        switch (errorCode)
+        {
+            case 1: // error connecting cafeBazaar
+            case 2: // error connecting cafeBazaar
+            case 4: // error connecting cafeBazaar
+            case 5: // error connecting cafeBazaar
+
+                break;
+            case 6: // user canceled the purchase
+
+                break;
+            case 7: // purchase failed
+
+                break;
+            case 8: // failed to consume product. but the purchase was successful.
+
+                break;
+            case 12: // error setup cafebazaar billing
+            case 13: // error setup cafebazaar billing
+            case 14: // error setup cafebazaar billing
+
+                break;
+            case 15: // you should enter your public key
+
+                break;
+            case 16: // unkown error happened
+
+                break;
+            case 17: // the result from cafeBazaar is not valid.
+
+                break;
+        }
+
+        txtResult.text = "errorCode: " + errorCode + ", " + info + "\n" + txtResult.text;
+        txtErrorDialog.text = "operation failed." + "\n" + info;
+        errorDialog.SetActive(true);
+    }
+
+    public void userHasThisProduct(Purchase purchase)
+    {
+        // user already has this product
+        txtResult.text = "user has product: " + purchase.productId + " , token: " + purchase.orderId + "\n" + txtResult.text;
+        switch (selectedProductIndex)
+        {
+            case 0: // 500 coin
+                saveCoin(500);
+                break;
+            case 1: // enable double coin
+                doubleCoin = true;
+                PlayerPrefs.SetInt("doubleCoin", 1);
+                btnDoubleCoin.interactable = false;
+                break;
+            default:
+                throw new UnassignedReferenceException("you forgot to give user the product after check inventory. product: " + purchase.productId);
+        }
+
+        txtSuccessDialog.text = "you had this product.";
+        successDialog.SetActive(true);
+    }
+
+    public void failToGetUserInventory(int errorCode, string info)
+    {
+        // user has not this product or some error happened
+        switch (errorCode)
+        {
+            case 3:  // error connecting cafeBazaar
+            case 10: // error connecting cafeBazaar
+
+                break;
+            case 9: // user didn't login to cafeBazaar
+
+                break;
+            case 11: // user has not this product
+
+                break;
+            case 12: // error setup cafebazaar billing
+            case 13: // error setup cafebazaar billing
+            case 14: // error setup cafebazaar billing
+
+                break;
+            case 15: // you should enter your public key
+
+                break;
+            case 16: // unkown error happened
+
+                break;
+            case 17: // the result from cafeBazaar is not valid.
+
+                break;
+        }
+        
+        txtResult.text = "errorCode: " + errorCode + ", " + info + "\n" + txtResult.text;
+        txtErrorDialog.text = "operation failed." + "\n" + info;
+        errorDialog.SetActive(true);
+    }
+
+    public void purchaseProduct(int productIndex)
+    {
+        selectedProductIndex = productIndex;
+        Product product = products[productIndex];
+        if (product.type == Product.ProductType.Consumable)
+        {
+            GetComponent<StoreHandler>().BuyAndConsume(product.productId);
+        }
+        else if (product.type == Product.ProductType.NonConsumable)
+        {
+            GetComponent<StoreHandler>().BuyProduct(product.productId);
+        }
+    }
+
+    public void checkIfUserHasProduct(int productIndex)
+    {
+        selectedProductIndex = productIndex;
+        GetComponent<StoreHandler>().CheckInventory(products[productIndex].productId);
+    }
+    
+    private void saveCoin(int value)
+    {
+        coin = PlayerPrefs.GetInt("coin", 0);
+        PlayerPrefs.SetInt("coin", coin + value);
+
+        StartCoroutine(animateCoinsText(coin, value));
+    }
+
+    private IEnumerator animateCoinsText(int coin, int value)
+    {
+
+        float part = (float)value / 20;
+
+        for (int i = 0; i < 20; i++)
+        {
+            int extra = (int)((i + 1) * part);
+            coinTxt.text = coin + extra + "$";
+            yield return new WaitForSecondsRealtime(0.03f);
+        }
+    }
+
+    public void addFreeCoin(int value)
+    {
+        if (doubleCoin) value *= 2;
+        saveCoin(value);
+    }
+}
+
---
+++ Assets/bobardo IAB/StoreHandler.cs
@@ -0,0 +1,178 @@
+﻿using UnityEngine;
+using SimpleJSON;
+using System;
+
+/* Apache License. Copyright (C) Bobardo Studio - All Rights Reserved.
+ * Unauthorized publishing the plugin with different name is strictly prohibited.
+ * This plugin is free and no one has right to sell it to others.
+ * http://bobardo.com
+ * http://opensource.org/licenses/Apache-2.0
+ */
+
+[RequireComponent (typeof(InAppStore))]
+public class StoreHandler : MonoBehaviour {
+
+    public string publicKey;
+    public string payload;
+
+    private AndroidJavaObject pluginUtilsClass = null;
+    private AndroidJavaObject activityContext = null;
+    
+    private void initiateBilling()
+    {
+        if (pluginUtilsClass == null)
+        {
+            using (AndroidJavaClass activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            {
+                activityContext = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
+            }
+
+            using (AndroidJavaClass pluginClass = new AndroidJavaClass("com.bobardo.bazaar.iab.ServiceBillingBazaar"))
+            {
+                if (pluginClass != null)
+                {
+
+                    pluginUtilsClass = pluginClass.CallStatic<AndroidJavaObject>("instance");
+                    pluginUtilsClass.CallStatic("setContext", activityContext);
+                    pluginUtilsClass.Call("setPublicKey", publicKey);
+                    pluginUtilsClass.Call("startIabServiceInBg");
+
+                }
+            }
+        }
+    }
+
+    public void BuyAndConsume(string produc_sku)
+    {
+        initiateBilling();
+
+        if (pluginUtilsClass != null)
+        {
+            pluginUtilsClass.Call("PurchaseAndConsume", produc_sku, payload);
+        }
+    }
+
+    public void BuyProduct(string produc_sku)
+    {
+        initiateBilling();
+
+        if (pluginUtilsClass != null)
+        {
+            pluginUtilsClass.Call("launchPurchaseFlow", produc_sku, payload);
+        }
+    }
+
+    public void CheckInventory(string produc_sku)
+    {
+        initiateBilling();
+
+        if (pluginUtilsClass != null)
+        {
+            pluginUtilsClass.Call("checkHasPurchase", produc_sku);
+        }
+    }
+
+    public void getPurchaseResult(string result)
+    {
+        if (result.Length == 0 || result == "" || result == null)
+        {
+            GetComponent<InAppStore>().purchasedFailed(16, "unknown error!!!");
+            return;
+        }
+        try
+        {
+            JSONNode purchaseResult = JSON.Parse(result);
+            int errorCode = purchaseResult["errorCode"].AsInt;
+            string data = purchaseResult["data"].Value.ToString();
+            if (errorCode == 0)
+            {
+                GetComponent<InAppStore>().purchasedSuccessful(getPurchaseData(data));
+            }
+            else
+            {
+                GetComponent<InAppStore>().purchasedFailed(errorCode, data);
+            }
+
+        }
+        catch
+        {
+            GetComponent<InAppStore>().purchasedFailed(17, "the result from cafeBazaar is not valid.");
+        }
+    }
+
+    public void getInventoryResult(string result)
+    {
+        if (result.Length == 0 || result == "" || result == null)
+        {
+            GetComponent<InAppStore>().purchasedFailed(16, "unknown error!!!");
+            return;
+        }
+        try
+        {
+            JSONNode purchaseResult = JSON.Parse(result);
+            int errorCode = purchaseResult["errorCode"].AsInt;
+            string data = purchaseResult["data"].Value.ToString();
+            if (errorCode == 0)
+            {
+                GetComponent<InAppStore>().purchasedSuccessful(getPurchaseData(data));
+            }
+            else
+            {
+                GetComponent<InAppStore>().purchasedFailed(errorCode, data);
+            }
+
+        }
+        catch
+        {
+            GetComponent<InAppStore>().purchasedFailed(17, "the result from cafeBazaar is not valid.");
+        }
+    }
+
+    private Purchase getPurchaseData(string data)
+    {
+        JSONNode info = JSONNode.Parse(data);
+        Purchase purchase = new Purchase();
+        purchase.orderId = info["orderId"].Value.ToString();
+        purchase.purchaseToken = info["purchaseToken"].Value.ToString();
+        purchase.payload = info["developerPayload"].Value.ToString();
+        purchase.packageName = info["packageName"].Value.ToString();
+        purchase.purchaseState = info["purchaseState"].AsInt;
+        purchase.purchaseTime = info["purchaseTime"].Value.ToString();
+        purchase.productId = info["productId"].Value.ToString();
+        purchase.json = data;
+        return purchase;
+    }
+
+    public void DebugLog(string msg)
+    {
+        Debug.Log(msg);
+    }
+
+    void OnApplicationQuit()
+    {
+        if (pluginUtilsClass != null)
+        {
+            pluginUtilsClass.Call("stopIabHelper");
+        }
+    }
+}
+
+public class Purchase
+{
+    public string orderId;
+    public string purchaseToken;
+    public string payload;
+    public string packageName;
+    public int purchaseState;
+    public string purchaseTime;
+    public string productId;
+    public string json;
+}
+
+[Serializable]
+public class Product
+{
+    public enum ProductType { Consumable, NonConsumable };
+    public string productId;
+    public ProductType type;
+}
